4568828e2bea813f2f2d6164e2a36f3a4672cedd
jhonyGri <jhonygrisales62@gmail.com>
2024-12-07 15:34:25 -0500

    MAYUSCULA

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Holaaaaa desde la API en express!");
+    res.send("Holaaaaa desde la API en expresSSs!");
 });
 
 app.listen(PORT, () => {
