c7714e6bb182e26a3b2a88c692414b599fb01054
jhonyGri <jhonygrisales62@gmail.com>
2024-12-07 17:59:15 -0500

    cambiando credenciales

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Holaaaaa desde la API en expresSSs!");
+    res.send("Holaaaaa desde la API en expresSSsss!");
 });
 
 app.listen(PORT, () => {
